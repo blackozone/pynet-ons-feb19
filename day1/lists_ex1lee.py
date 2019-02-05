85268a6100daafd6e7538e6ef4e9bdceac50e09e
Applied Python user <blackozone@gmail.com>
2019-02-05 12:51:53 -0800

    My attemmpt at list tampering.

---
+++ day1/lists_ex1lee.py
@@ -0,0 +1,12 @@
+my_list = ["non", "naan", "nyan", "nine", "nein"]
+print(my_list)
+my_list.append("gucci")
+my_list.append("prada")
+
+print(my_list.pop(0))
+
+print("Length of list: {}".format(len(my_list)))
+my_list.sort()
+print(my_list)
+
+
