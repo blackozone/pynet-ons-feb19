d1786cd4729f8b8b5f554e1f791e8208395cb96e
Applied Python user <blackozone@gmail.com>
2019-02-05 09:56:06 -0800

    My take on the exercise.

---
+++ day1/str_ex1lee.py
@@ -0,0 +1,9 @@
+name1 = "Lee"
+name2 = "Greg"
+name3 = "Rudy"
+
+print("{:>30}{:>30}{:>30}".format(name1, name2, name3))
+
+name4 = input("Submit another for the sacrifice: ")
+print("{:>30}".format (name4))
+
